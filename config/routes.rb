4fbc2333173f7fc352f568490af6348e2574402d
erikyoung <erik.young@rmit.edu.vn>
2017-06-02 14:51:15 +0700

    changes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users do
-  	get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  devise_for :users
   resources :articles do	
   	resources :comments
   end
