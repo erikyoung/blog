f00dad52e8aa1ac2d1a3095c3601346414c5f467
erikyoung <erik.young@rmit.edu.vn>
2017-06-02 16:22:53 +0700

    testing crash of article delete

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
 	belongs_to :user, dependent: :destroy
 	validates :title, presence: true
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	has_many :taggings
 	has_many :tags, through: :taggings
 	def self.search(search)
